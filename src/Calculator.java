f1409dc82f7745b8afd5b0f55490cf4b2a2f6780
Aaron Langley <alangley345@gmail.com>
2022-08-12 17:09:45 -0400

    Added buttons and display

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,44 +1,47 @@
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.*;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
 public class Calculator {
 
 	public static void main(String[] args) {
+		//build window and place for buttons
 		Display display = new Display();
 		Shell shell     = new Shell(display);
 		shell.setText("Java Calculator");
 		shell.setSize(400,500);
 		
+		//composite for buttons
+		shell.setLayout(new GridLayout());
+	    final Composite composite = new Composite(shell, SWT.NONE);
+	    GridLayout grid = new GridLayout();
+	    grid.numColumns = 3;
+	    composite.setLayout(grid);
+	    
+	    Text disWindow = new Text(composite, 0);
+	    GridData gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+	    gridData.horizontalSpan = 3;
+	    disWindow.setLayoutData(gridData);
+
+	 
+	    
+	    //adding buttons
+	    int buttonX    = 10;
+	    int buttonY    = 10;
+	    int buttonSize = 25;
+	    for (int i=9; i>=0; i--) {
+	      Button b = new Button(composite, SWT.PUSH);
+	      b.setSize(buttonSize, buttonSize);
+	      b.setText(String.valueOf(i));
+	    }
 		
-		final Button Button7 = new Button(shell,SWT.PUSH);
-		Button7.setText("7");
-		Button7.pack();
-		
-		final Button Button8 = new Button(shell,SWT.PUSH);
-		Button8.setText("8");
-		Button8.pack();
-		
-		final Button Button9 = new Button(shell,SWT.PUSH);
-		Button9.setText("9");
-		Button9.pack();
-		/*...
-		final Button Button1 = new Button(shell,SWT.PUSH);
-		Button1.setText("7");
-		Button1.pack();
-		
-		final Button Button1 = new Button(shell,SWT.PUSH);
-		Button1.setText("7");
-		Button1.pack();
-		
-		final Button Button1 = new Button(shell,SWT.PUSH);
-		Button1.setText("7");
-		Button1.pack();
-		
-		final Button Button1 = new Button(shell,SWT.PUSH);
-		Button1.setText("7");
-		Button1.pack();
-		...*/
-		
+	    
+	    
 		shell.open();
 		while(!shell.isDisposed())
 			if (!display.readAndDispatch())
