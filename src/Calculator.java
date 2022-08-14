89f5e18212bc3d57d3a66ced4a31294d66f7b21b
Aaron Langley <alangley345@gmail.com>
2022-08-14 11:30:01 -0400

    Created num button class

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,59 +9,7 @@ import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
-public class Calculator {
-	
-	private static Shell makeShell(String title, int width, int height) {
-		//build window and place for buttons
-		Display display = new Display();
-		Shell shell     = new Shell(display);
-		shell.setText(title);
-		shell.setSize(width,height);
-		shell.setLayout(new GridLayout());
-		
-		shell.open();
-		while(!shell.isDisposed())
-			if (!display.readAndDispatch())
-				display.sleep();
-		display.dispose();
-		
-		return shell;
-	}
-	
-	private static Composite makeComposite(Shell shell) {
-		
-		final Composite composite = new Composite(shell, SWT.NONE);
-		GridLayout grid = new GridLayout();
-	  	grid.numColumns = 3;
-		composite.setLayout(grid);
-		
-		return composite;
-	}
-	
-    private static void button1(Composite composite, Text window) {
-    	Button one = new Button(composite,SWT.PUSH);
-	  	one.setText("1");
-	  	
-	  	one.addListener(SWT.Selection, (Listener) new Listener()
-	  	{
-	  	    public void handleEvent(Event event)
-	  	    {
-	  	        window.setText("1");
-	  	    }
-	  	});
-    }
-	
-    private static Text makeDisplay(Shell shell) {
-    	//display
-        GridData gridData = new GridData(SWT.FILL, SWT.BEGINNING, true, false);
-        gridData.horizontalSpan = 2;
-        Text disWindow = new Text(shell, 0);
-        disWindow.setLayoutData(gridData);
-        disWindow.setText("Hello");
-        
-        return disWindow;
-    }
-    
+public class Calculator {    
     //adding buttons
     //int buttonX    = 10;
     //int buttonY    = 10;
@@ -73,12 +21,63 @@ public class Calculator {
     //  
     //}
   	
-
 	public static void main(String[] args) {
-		Shell shell         = makeShell("Java Calculator", 400, 500);
-		Composite composite = makeComposite(shell);
-		Text display        = makeDisplay(shell);
-
+		String title        = "Java Calculator";
+		int    width        = 400;
+		int    height       = 500;
+		float  valueHolder  = 0;
+		
+		Display display = new Display();
+		Shell shell     = new Shell(display);
+		shell.setText(title);
+		shell.setSize(width,height);
+		shell.setLayout(new GridLayout());
+		
+        GridData gridData = new GridData(SWT.FILL, SWT.BEGINNING, true, false);
+        gridData.horizontalSpan = 2;
+        Text disWindow = new Text(shell, 0);
+        disWindow.setLayoutData(gridData);
+        disWindow.setText("");
+		
+		//composite for buttons
+		final Composite composite = new Composite(shell, SWT.NONE);
+		GridLayout grid = new GridLayout();
+	  	grid.numColumns = 3;
+		composite.setLayout(grid);
+		
+		NumberButton button9 = new NumberButton(composite, disWindow, "9");
+		NumberButton button8 = new NumberButton(composite, disWindow, "8");
+		NumberButton button7 = new NumberButton(composite, disWindow, "7");
+		NumberButton button6 = new NumberButton(composite, disWindow, "6");
+		NumberButton button5 = new NumberButton(composite, disWindow, "5");
+		NumberButton button4 = new NumberButton(composite, disWindow, "4");
+		NumberButton button3 = new NumberButton(composite, disWindow, "3");
+		NumberButton button2 = new NumberButton(composite, disWindow, "2");
+		NumberButton button1 = new NumberButton(composite, disWindow, "1");
+		NumberButton button0 = new NumberButton(composite, disWindow, "0");
+		
+		Button plus = new Button(composite,SWT.PUSH);
+		plus.setText("+");
+	  	plus.addListener(SWT.Selection, (Listener) new Listener()
+	  	{
+	  		float tempValue;
+	  	    public final void handleEvent(Event event)
+	  	    {
+	  	    	if (disWindow.getText() != "") {
+	  	    		tempValue = Float.parseFloat(disWindow.getText());
+	  	    		disWindow.setText("");
+	  	    		float valueHolder = tempValue;
+	  		  	    String s = String.valueOf(valueHolder);  
+	  		  	    System.out.println(s);
+		  	    }
+	  	    }
+	  	});
+		
+		shell.open();
+		while(!shell.isDisposed())
+			if (!display.readAndDispatch())
+				display.sleep();
+		display.dispose();
 	}
 
 }
---
+++ src/NumberButton.java
@@ -0,0 +1,22 @@
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Text;
+
+public class NumberButton {
+	
+	public NumberButton(Composite composite, Text display, String buttonLabel){
+		Button button = new Button(composite,SWT.PUSH);
+		button.setText(buttonLabel);
+	  	button.addListener(SWT.Selection, (Listener) new Listener()
+	  	{
+	  	    public void handleEvent(Event event)
+	  	    {
+	  	    	
+	  	        display.setText(display.getText() + buttonLabel);
+	  	    }
+	  	});
+	}
+}
